679c460d4cb8710e3d6c2fd69b32a5e90c2e778d
caydende <96990579+caydende@users.noreply.github.com>
2025-07-08 05:53:18 +0300

    added animation when page loads

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,7 +1,10 @@
+import { isPlatformBrowser } from '@angular/common';
 import { RandomQuote } from './../../CORE/services/randomQuoteService/random-quote';
-import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal, viewChild, viewChildren, ElementRef, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import { FlowbiteService } from '../../CORE/services/flow-bite';
 import { initFlowbite } from 'flowbite';
+import gsap from 'gsap';
+
 
 @Component({
   selector: 'app-home-page',
@@ -9,19 +12,67 @@ import { initFlowbite } from 'flowbite';
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
-export class HomePage implements OnInit {
+export class HomePage implements AfterViewInit {
   private readonly flowbiteService = inject(FlowbiteService)
   private readonly randomQuoteService = inject(RandomQuote)
+  private readonly platform_id  = inject(PLATFORM_ID)
 
+  animatedR = viewChildren<ElementRef>('animateR')
+  animatedL = viewChild<ElementRef>('animateL')
 
   QuoteData:WritableSignal<any> = signal({})
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.flowbiteService.loadFlowbite((flowbite) => {
       initFlowbite();
     });
-
+    
     this.randomQuote()
+    if(isPlatformBrowser(this.platform_id)){
+      this.animateFadeIn()
+    }
+  }
+
+
+  animateFadeIn(): void {
+    this.animateFromRight();
+    this.animateFromLeft();
+  }
+  
+  private animateFromRight(): void {
+    const animateList = this.animatedR();
+
+      const elements = animateList.map(ref => ref.nativeElement);
+  
+      gsap.set(elements, {
+        x: -300,
+        opacity: 0,
+      });
+      gsap.to(elements, {
+        x: 0,
+        opacity: 1,
+        duration: 2,
+        ease: "power1.out",
+        stagger: 0.2,
+      });
+  }
+  
+  private animateFromLeft(): void {
+    const imgEl = this.animatedL()?.nativeElement;
+    if (imgEl) {
+      // Set initial state
+      gsap.set(imgEl, {
+        x: 300,
+        opacity: 0,
+      });
+
+      gsap.to(imgEl, {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power1.out",
+      });
+    }
   }
 
 
--- src/app/pages/navbar/navbar.ts
+++ src/app/pages/navbar/navbar.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, AfterViewInit } from '@angular/core';
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother,TextPlugin);
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +14,28 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
-export class Navbar {
+export class Navbar implements AfterViewInit {
+  nav = viewChild<ElementRef>('navBar');
 
+  ngAfterViewInit(): void {
+    this.animateNavbarFadeIn();
+  }
+
+  animateNavbarFadeIn(): void {
+    const navEl = this.nav()?.nativeElement;
+    if (navEl) {
+      gsap.fromTo(navEl, 
+        {
+          opacity: 0,
+          y:-100
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: .7,
+          ease: "power1.out",
+        }
+      );
+    }
+  }
 }
